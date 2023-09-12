5e3db690a3b7e9a7607ed796d6bf950d0dacb3da
hyohyo814 <sojaisam@gmail.com>
2023-09-12 16:22:53 -0700

    working on CRUD

--- index.tsx
+++ index.tsx
@@ -1,19 +1,49 @@
 import html from "@elysiajs/html";
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import * as elements from "typed-html";
 
 const app = new Elysia()
   .use(html())
   .get("/", ({ html }) => html(
     <BaseHtml>
-      <body class="flex w-full h-screen justify-center items-center text-4xl">
-        <button hx-post="/clicked" hx-swap="outerHTML">
-          button
-        </button>
-      </body>
+      <body
+        class="flex w-full h-screen justify-center items-center text-4xl"
+        hx-get="/todos"
+        hx-trigger="load"
+        hx-swap="innerHtml"
+      />
     </BaseHtml>
   ))
-  .post("/clicked", () => <div class="text-sky-500 text-4xl">Server response</div>)
+  .get("/todos", () => <TodoList todos={db} />)
+  .post(
+    "/todos/toggle/:id",  
+    ({ params }) => {
+      const todo = db.find(todo => todo.id === params.id);
+      if (!!todo) {
+        todo.completed = !todo.completed;
+        return <TodoItem {...todo} />;
+      }
+    },
+    {
+      params: t.Object({
+        id: t.Numeric(),
+      }),
+    }
+  )
+  .delete(
+    "/todos/:id",
+    ({ params }) => {
+      const todo = db.find(todo => todo.id === params.id);
+      if (todo) {
+        db.splice(db.indexOf(todo), 1);
+      }
+    },
+    {
+      params: t.Object({
+        id: t.Numeric(),
+      }),
+    }
+  )
   .listen(3000);
 
 console.log(`Elysia running at http://${app.server?.hostname}:${app.server?.port}`);
@@ -48,8 +78,21 @@ function TodoItem({ content, completed, id }: Todo) {
   return (
     <div class="flex flex-row space-x-3">
       <p>{content}</p>
-      <input type="checkbox" checked={completed} />
-      <button class="text-red-500">X</button>
+      <input
+        type="checkbox"
+        checked={completed}
+        hx-post={`/todos/toggle/${id}`}
+        hx-target="closest div"
+        hx-swap="outerHTML"
+      />
+      <button
+        class="text-red-500"
+        hx-delete={`/todos/${id}`}
+        hx-swap="outerHTML"
+        hx-target="closest div"
+      >
+        X
+      </button>
     </div>
   )
 }
